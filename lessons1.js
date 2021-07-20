2b8404475bb0681d9beea5a57aab253eb059f0d5
nazarko310 <nazar2000pok@gmail.com>
2021-07-20 20:39:51 +0300

    ts_lessons1_practice

--- lessons1.js
+++ lessons1.js
@@ -1,131 +1,50 @@
-function userArrayList(user) {
-    for (var _i = 0, user_1 = user; _i < user_1.length; _i++) {
-        var userElement = user_1[_i];
-        console.log(userElement);
-    }
-}
-userArrayList([
-    {
-        name: 'Vasia',
-        age: 12
-    }
-]);
-function setCar(car) {
-    // console.log(car);
-}
-setCar({
-    model: 'audi', speed: 320, power: 270
-});
-var Audi = /** @class */ (function () {
-    function Audi(model, power, speed, sport) {
-        this.model = model;
-        this.power = power;
-        this.speed = speed;
-        this.sport = sport;
-    }
-    return Audi;
-}());
-var BMW = /** @class */ (function () {
-    function BMW(model, power, speed, check) {
-        this.model = model;
-        this.power = power;
-        this.speed = speed;
-        this.check = check;
-    }
-    return BMW;
-}());
-var cars = [
-    new Audi('a4', 240, 300, true),
-    new BMW('x5', 220, 350, false)
-];
-var carAudi = cars[0];
-console.log(carAudi.sport);
-var SchoolOfMath = /** @class */ (function () {
-    function SchoolOfMath(artHall, countOfChildren, countOfClass, gym, name, competitions) {
-        this.artHall = artHall;
-        this.countOfChildren = countOfChildren;
-        this.countOfClass = countOfClass;
-        this.gym = gym;
-        this.name = name;
-        this.competitions = competitions;
-    }
-    return SchoolOfMath;
-}());
-var SchoolOfArt = /** @class */ (function () {
-    function SchoolOfArt(artHall, countOfChildren, countOfClass, gym, name, artRoom) {
-        this.artHall = artHall;
-        this.countOfChildren = countOfChildren;
-        this.countOfClass = countOfClass;
-        this.gym = gym;
-        this.name = name;
-        this.artRoom = artRoom;
-    }
-    return SchoolOfArt;
-}());
-var schools = [
-    new SchoolOfMath(false, 120, 5, true, 'SchoolOfMath', true),
-    new SchoolOfArt(true, 500, 20, false, 'SchoolOfArt', true)
-];
-var competition = schools[0];
-console.log(competition.competitions);
-var Users = /** @class */ (function () {
-    function Users(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    Users.prototype.ageGrow = function (newAge) {
-        if (newAge > 0 && newAge < 100 && newAge != this.age) {
-            this.age = newAge;
+// Создать интерфейс по такой объект:
+[{
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
         }
-    };
-    return Users;
-}());
-var user = new Users('Vasia', 15);
-user.ageGrow(20);
-console.log(user);
-var Laptop = /** @class */ (function () {
-    function Laptop(CPU, OZU, batteryPower) {
-        this.CPU = CPU;
-        this.OZU = OZU;
-        this.batteryPower = batteryPower;
-    }
-    Laptop.prototype.batteryPowerImprove = function (batteryImprove) {
-        if (batteryImprove > this.batteryPower && batteryImprove > 20000)
-            this.batteryPower = batteryImprove;
-    };
-    return Laptop;
-}());
-var PC = /** @class */ (function () {
-    function PC(CPU, OZU, game) {
-        this.CPU = CPU;
-        this.OZU = OZU;
-        this.game = game;
-    }
-    return PC;
-}());
-var computers = [
-    new Laptop('Intel', 8, 20000),
-    new PC('AMD', 16, true)
-];
-var laptop = computers[0];
-laptop.batteryPowerImprove(30000);
-console.log(laptop);
-var Gender;
-(function (Gender) {
-    Gender["MALE"] = "male";
-    Gender["FEMALE"] = "female";
-})(Gender || (Gender = {}));
-function genderSet(gender) {
-    console.log(gender);
+    }];
+function employeeOnTheWork(employee) {
+    console.log(employee);
 }
-genderSet(Gender.MALE);
-var Size;
-(function (Size) {
-    Size["S"] = "S";
-    Size["M"] = "M";
-    Size["L"] = "L";
-})(Size || (Size = {}));
-function sizeOfClothes(sizeOfClothe) {
-    console.log(sizeOfClothe);
-}
-sizeOfClothes(Size.S);
+employeeOnTheWork({
+    id: 1,
+    name: 'Leanne Graham',
+    username: "Bret",
+    email: 'Sincere@april.biz',
+    address: {
+        street: 'Kulas Light',
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+        geo: {
+            lat: -37.3159,
+            lng: 81.1496
+        }
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets'
+    }
+});
--- lessons1.ts
+++ lessons1.ts
@@ -1,219 +1,79 @@
-function userArrayList(user: [{ name: string, age: number }]) {
-    for (const userElement of user) {
-
-        console.log(userElement);
-    }
-
-}
-
-userArrayList([
-
-        {
-            name: 'Vasia',
-            age: 12
+// Создать интерфейс по такой объект:
+// [{
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "address": {
+//         "street": "Kulas Light",
+//         "suite": "Apt. 556",
+//         "city": "Gwenborough",
+//         "zipcode": "92998-3874",
+//         "geo": {
+//             "lat": "-37.3159",
+//             "lng": "81.1496"
+//         }
+//     },
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//     "company": {
+//         "name": "Romaguera-Crona",
+//         "catchPhrase": "Multi-layered client-server neural-net",
+//         "bs": "harness real-time e-markets"
+//     }
+// }]
+interface Employee {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: {
+        street: string,
+        suite: string,
+        city: string,
+        zipcode: string
+        geo: {
+            lat: number,
+            lng: number
         }
-    ]
-)
-
-
-interface Car {
-    model: string,
-    power: number,
-    speed: number
+    },
+    phone: string,
+    website: string,
+    company: {
+        name: string,
+        catchPhrase: string,
+        bs: string
+    }
 }
 
-function setCar(car: Car) {
-    // console.log(car);
+
+function employeeOnTheWork(employee: Employee) {
+    console.log(employee);
 }
 
-setCar({
-    model: 'audi', speed: 320, power: 270
-
+employeeOnTheWork({
+    id: 1,
+    name: 'Leanne Graham',
+    username: "Bret",
+    email: 'Sincere@april.biz',
+    address: {
+        street: 'Kulas Light',
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+        geo: {
+            lat: -37.3159,
+            lng: 81.1496
+        }
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets'
+    }
 })
 
-class Audi implements Car {
-    constructor(model: string, power: number, speed: number, sport: boolean) {
-        this.model = model;
-        this.power = power;
-        this.speed = speed;
-        this.sport = sport;
-    }
-
-    model: string;
-    power: number;
-    speed: number;
-    sport: boolean
-}
-
-class BMW implements Car {
-    model: string;
-    power: number;
-    speed: number;
-    check: boolean
-
-    constructor(model: string, power: number, speed: number, check: boolean) {
-        this.model = model;
-        this.power = power;
-        this.speed = speed;
-        this.check = check;
-    }
-}
-
-let cars: Car[] = [
-    new Audi('a4', 240, 300, true),
-    new BMW('x5', 220, 350, false)
-]
-
-let carAudi = cars[0] as Audi;
-console.log(carAudi.sport);
 
 
-interface School {
-    countOfClass: number,
-    name: string,
-    countOfChildren: number,
-    gym: boolean,
-    artHall: boolean
-}
-
-class SchoolOfMath implements School {
-    artHall: boolean;
-    countOfChildren: number;
-    countOfClass: number;
-    gym: boolean;
-    name: string;
-    competitions: boolean
-
-    constructor(artHall: boolean, countOfChildren: number, countOfClass: number, gym: boolean, name: string, competitions: boolean) {
-        this.artHall = artHall;
-        this.countOfChildren = countOfChildren;
-        this.countOfClass = countOfClass;
-        this.gym = gym;
-        this.name = name;
-        this.competitions = competitions;
-    }
-}
-
-
-class SchoolOfArt implements School {
-    artHall: boolean;
-    countOfChildren: number;
-    countOfClass: number;
-    gym: boolean;
-    name: string;
-    artRoom: boolean
-
-    constructor(artHall: boolean, countOfChildren: number, countOfClass: number, gym: boolean, name: string, artRoom: boolean) {
-        this.artHall = artHall;
-        this.countOfChildren = countOfChildren;
-        this.countOfClass = countOfClass;
-        this.gym = gym;
-        this.name = name;
-        this.artRoom = artRoom;
-    }
-}
-
-let schools: School[] = [
-    new SchoolOfMath(false, 120, 5, true, 'SchoolOfMath', true),
-    new SchoolOfArt(true, 500, 20, false, 'SchoolOfArt', true)
-]
-
-let competition = schools[0] as SchoolOfMath;
-
-console.log(competition.competitions);
-
-
-class Users {
-    name: string
-    private age: number
-
-    ageGrow(newAge) {
-        if (newAge > 0 && newAge < 100 && newAge != this.age) {
-            this.age = newAge
-        }
-    }
-
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-}
-
-let user = new Users('Vasia', 15);
-user.ageGrow(20);
-console.log(user);
-
-
-interface Computer {
-    CPU: string,
-    OZU: number,
-}
-
-class Laptop implements Computer {
-    CPU: string;
-    OZU: number;
-    private batteryPower: number
-
-
-    constructor(CPU: string, OZU: number, batteryPower: number) {
-        this.CPU = CPU;
-        this.OZU = OZU;
-        this.batteryPower = batteryPower;
-    }
-
-    batteryPowerImprove(batteryImprove) {
-        if (batteryImprove > this.batteryPower && batteryImprove > 20000)
-            this.batteryPower = batteryImprove
-    }
-}
-
-class PC implements Computer {
-    constructor(CPU: string, OZU: number, game: boolean) {
-        this.CPU = CPU;
-        this.OZU = OZU;
-        this.game = game;
-    }
-
-    CPU: string;
-    OZU: number;
-    game: boolean
-}
-
-
-let computers: Computer[] = [
-    new Laptop('Intel', 8, 20000),
-    new PC('AMD', 16, true)
-
-]
-
-let laptop = computers[0] as Laptop
-laptop.batteryPowerImprove(30000)
-console.log(laptop);
-
-
-enum Gender {
-    MALE = 'male',
-    FEMALE = 'female'
-}
-
-
-function genderSet(gender: Gender) {
-    console.log(gender)
-}
-
-genderSet(Gender.MALE)
-
-
-enum Size {
-    S = 'S',
-    M = 'M',
-    L = 'L'
-}
-
-
-function sizeOfClothes(sizeOfClothe: Size) {
-    console.log(sizeOfClothe)
-}
-
-
-sizeOfClothes(Size.S)
